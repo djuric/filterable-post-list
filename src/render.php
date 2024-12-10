0855c7790251e5ade9862befedd432625fcb72d7
Zarko <zarko.dorian@gmail.com>
2024-12-10 22:01:03 +0100

    Add markup and interactivity

--- src/render.php
+++ src/render.php
@@ -10,47 +10,135 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-// Generates a unique id for aria-controls.
-$unique_id = wp_unique_id( 'p-' );
+$unique_id = wp_unique_id();
 
-// Adds the global state.
-wp_interactivity_state(
-	'create-block',
-	array(
-		'isDark'    => false,
-		'darkText'  => esc_html__( 'Switch to Light', 'filterable-post-list' ),
-		'lightText' => esc_html__( 'Switch to Dark', 'filterable-post-list' ),
-		'themeText'	=> esc_html__( 'Switch to Dark', 'filterable-post-list' ),
-	)
+$page_query_var     = "filterable-post-list-page-{$unique_id}";
+$category_query_var = "filterable-post-list-category-{$unique_id}";
+$tags_query_var     = "filterable-post-list-tags-{$unique_id}";
+
+$current_page = isset( $_GET[ $page_query_var ] ) ? (int) $_GET[ $page_query_var ] : 1;
+
+$args = [
+	'post_type'      => 'post',
+	'posts_per_page' => 5,
+	'orderby'        => 'date',
+	'order'          => 'DESC',
+	'paged'          => $current_page,
+	'tax_query'      => [],
+];
+
+if ( isset( $_GET[ $category_query_var ] ) && ! empty( $_GET[ $category_query_var ] ) ) {
+	$args['tax_query'][] = [
+		'taxonomy' => 'category',
+		'terms'    => $_GET[ $category_query_var ],
+	];
+}
+
+if ( isset( $_GET[ $tags_query_var ] ) && ! empty( $_GET[ $tags_query_var ] ) ) {
+	$args['tax_query'][] = [
+		'taxonomy' => 'post_tag',
+		'terms'    => explode( ',', $_GET[ $tags_query_var ] ),
+		'operator' => 'IN',
+	];
+}
+
+$query = new WP_Query( $args );
+
+$tags = get_tags(
+	[
+		'hide_empty' => 0,
+	]
 );
+
+$categories = get_categories();
+
+$selected_category = isset( $_GET[ $category_query_var ] ) ? (int) $_GET[ $category_query_var ] : 0;
+$selected_tags     = isset( $_GET[ $tags_query_var ] ) ? array_map( 'absint', explode( ',', $_GET[ $tags_query_var ] ) ) : [];
+
 ?>
 
 <div
 	<?php echo get_block_wrapper_attributes(); ?>
 	data-wp-interactive="create-block"
-	<?php echo wp_interactivity_data_wp_context( array( 'isOpen' => false ) ); ?>
-	data-wp-watch="callbacks.logIsOpen"
-	data-wp-class--dark-theme="state.isDark"
+	<?php
+	echo wp_interactivity_data_wp_context(
+		[
+			'selectedCategory' => $selected_category,
+			'selectedTags'     => $selected_tags,
+			'categoryQueryVar' => $category_query_var,
+			'tagsQueryVar'     => $tags_query_var,
+			'pageQueryVar'     => $page_query_var,
+		]
+	);
+	?>
 >
-	<button
-		data-wp-on--click="actions.toggleTheme"
-		data-wp-text="state.themeText"
-	></button>
+	<div class="wp-block-create-block-filterable-post-list-filters">
+		<div class="wp-block-create-block-filterable-post-list-filters__category">
+			<label><?php esc_html_e( 'Category:', 'filterable-post-list' ); ?></label>
+			<select data-wp-on--change="actions.updateCategory">
+				<option value="0"><?php esc_html_e( '- All -', 'filterable-post-list' ); ?></option>
+				<?php foreach ( $categories as $cat ) { ?>
+				<option value="<?php echo $cat->term_id; ?>" <?php selected( $cat->term_id, $selected_category ); ?>><?php echo $cat->name; ?></option>
+				<?php } ?>
+			</select>
+		</div>
+		
+		<div class="wp-block-create-block-filterable-post-list-filters__tags">
+			<label><?php esc_html_e( 'Tags:', 'filterable-post-list' ); ?></label>
+			<div class="wp-block-create-block-filterable-post-list-filters__tags-container">
+				<?php foreach ( $tags as $tag ) { ?>
+				<label class="wp-block-create-block-filterable-post-list-filters__tag-item">
+					<input type="checkbox" data-wp-on--change="actions.updateTags" name="tag_id" value="<?php echo $tag->term_id; ?>" <?php checked( in_array( $tag->term_id, $selected_tags ) ); ?> />
+					<?php echo $tag->name; ?>
+				</label>
+				<?php } ?>
+			</div>
+		</div>
+	</div>
 
-	<button
-		data-wp-on--click="actions.toggleOpen"
-		data-wp-bind--aria-expanded="context.isOpen"
-		aria-controls="<?php echo esc_attr( $unique_id ); ?>"
+	<div
+	data-wp-interactive="create-block"
+	class="wp-block-create-block-filterable-post-list-posts"
+	<?php echo wp_interactivity_data_wp_context( [ 'pageQueryVar' => $page_query_var ] ); ?> 
+	data-wp-router-region="filterable-post-list-<?php echo $unique_id; ?>"
 	>
-		<?php esc_html_e( 'Toggle', 'filterable-post-list' ); ?>
-	</button>
+		<p><?php printf( __( 'Total results: %s', 'filterable-post-list' ), '<i>' . $query->found_posts . '</i>' ); ?></p>
+		<div class="wp-block-create-block-filterable-post-list-posts__container">
+			<?php
+			if ( $query->have_posts() ) :
+				while ( $query->have_posts() ) :
+					$query->the_post();
+					?>
+			<div class="wp-block-create-block-filterable-post-list-posts__item">
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			</div>
+					<?php
+				endwhile;
+			else :
+				?>
+			<div class="wp-block-create-block-filterable-post-list-posts__item">
+				<p><?php esc_html_e( 'No results found.', 'filterable-post-list' ); ?></p>
+			</div>
+				<?php
+			endif;
+			wp_reset_postdata();
+			?>
+		</div>
 
-	<p
-		id="<?php echo esc_attr( $unique_id ); ?>"
-		data-wp-bind--hidden="!context.isOpen"
-	>
-		<?php
-			esc_html_e( 'Filterable Post List - hello from an interactive block!', 'filterable-post-list' );
-		?>
-	</p>
+		<?php if ( $query->have_posts() ) : ?>
+		<div class="wp-block-create-block-filterable-post-list-posts__navigation">
+			<div class="wp-block-create-block-filterable-post-list-posts__navigation--previous">
+			<?php if ( $current_page > 1 ) : ?>
+				<a href="<?php echo add_query_arg( [ $page_query_var => $current_page - 1 ] ); ?>" data-wp-on--click="actions.previous"><?php esc_html_e( '&laquo; Previous', 'filterable-post-list' ); ?></a>
+			<?php endif; ?>
+			</div>
+
+			<div class="wp-block-create-block-filterable-post-list-posts__navigation--next">
+			<?php if ( $current_page < $query->max_num_pages ) : ?>
+				<a href="<?php echo add_query_arg( [ $page_query_var => $current_page + 1 ] ); ?>" data-wp-on--click="actions.next"><?php esc_html_e( 'Next &raquo;', 'filterable-post-list' ); ?></a>
+			<?php endif; ?>
+			</div>
+		</div>
+		<?php endif; ?>
+	</div>
 </div>
--- src/view.js
+++ src/view.js
@@ -3,26 +3,102 @@
  */
 import { store, getContext } from '@wordpress/interactivity';
 
-const { state } = store( 'create-block', {
-	state: {
-		get themeText() {
-			return state.isDark ? state.darkText : state.lightText;
-		},
-	},
+store( 'create-block', {
 	actions: {
-		toggleOpen() {
-			const context = getContext();
-			context.isOpen = ! context.isOpen;
+		*next( e ) {
+			e.preventDefault();
+			const ctx = getContext();
+			const url = new URL( window.location );
+
+			const currentPage = +url.searchParams.get( ctx.pageQueryVar );
+			let nextPage = 2;
+
+			if ( ! isNaN( currentPage ) && currentPage !== 0 ) {
+				nextPage = currentPage + 1;
+			}
+
+			url.searchParams.set( ctx.pageQueryVar, nextPage );
+
+			const { actions } = yield import(
+				'@wordpress/interactivity-router'
+			);
+			yield actions.navigate(
+				`${ window.location.pathname }${ url.search }`
+			);
 		},
-		toggleTheme() {
-			state.isDark = ! state.isDark;
+		*previous( e ) {
+			e.preventDefault();
+			const ctx = getContext();
+			const url = new URL( window.location );
+
+			const currentPage = +url.searchParams.get( ctx.pageQueryVar );
+			let previousPage = 1;
+
+			if ( ! isNaN( currentPage ) && currentPage !== 0 ) {
+				previousPage = currentPage - 1;
+			}
+
+			url.searchParams.set( ctx.pageQueryVar, previousPage );
+
+			const { actions } = yield import(
+				'@wordpress/interactivity-router'
+			);
+			yield actions.navigate(
+				`${ window.location.pathname }${ url.search }`
+			);
 		},
-	},
-	callbacks: {
-		logIsOpen: () => {
-			const { isOpen } = getContext();
-			// Log the value of `isOpen` each time it changes.
-			console.log( `Is open: ${ isOpen }` );
+		*updateCategory( e ) {
+			const ctx = getContext();
+			const categoryId = e.target.value;
+			const url = new URL( window.location );
+
+			// Update URL with new category ID and reset pagination.
+			url.searchParams.set( ctx.categoryQueryVar, categoryId );
+			url.searchParams.set( ctx.pageQueryVar, 1 );
+
+			// Update local context.
+			ctx.selectedCategory = +categoryId;
+
+			const { actions } = yield import(
+				'@wordpress/interactivity-router'
+			);
+			yield actions.navigate(
+				`${ window.location.pathname }${ url.search }`
+			);
+		},
+		*updateTags( e ) {
+			e.preventDefault();
+			const ctx = getContext();
+			const tagId = e.target.value;
+			const url = new URL( window.location );
+
+			const existingTags = url.searchParams.get( ctx.tagsQueryVar );
+
+			// Create an array out of existing tag IDs from URL.
+			const tagIds = existingTags
+				? existingTags.split( ',' ).map( ( id ) => id.trim() )
+				: [];
+
+			// Toggle tag ID in array.
+			if ( ! tagIds.includes( tagId ) ) {
+				tagIds.push( tagId );
+			} else {
+				tagIds.splice( tagIds.indexOf( tagId ), 1 );
+			}
+
+			// Update URL with new tag IDs and reset pagination.
+			url.searchParams.set( ctx.tagsQueryVar, tagIds.join( ',' ) );
+			url.searchParams.set( ctx.pageQueryVar, 1 );
+
+			// Update local context.
+			ctx.selectedTags = tagIds;
+
+			const { actions } = yield import(
+				'@wordpress/interactivity-router'
+			);
+			yield actions.navigate(
+				`${ window.location.pathname }${ url.search }`
+			);
 		},
 	},
 } );
